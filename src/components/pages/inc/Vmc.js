6bd8ee554150d9e8c78b20e4db9b709d5a09a6b7
abuXahra <abdulmuminisah79@gmail.com>
2022-06-27 11:45:38 +0100

    website updated

--- src/components/pages/inc/Vmc.js
+++ src/components/pages/inc/Vmc.js
@@ -2,10 +2,27 @@ import React from 'react'
 import TextComponent from '../../inc/TextComponent'
 
 const Vmc = () => {
-    const vmcText = [
-        "Nigeria is a country where million number of students graduates yearly and when it comes to issue of employment, only 20% of them got employed and there is no assurance of job for the rest of the 80% and that sequence continue",
-        "Skill is a powerful instrument in the sense that, it equipped or empower people and improves their social acceptance. Skill is a something that is complement by a good economic growth and improve job employment. Nigeria is moving towardsthe verge of being a developed country with the needs for a greater number of skillful and intelligent people",
-        "Unskilled employ can diminish the success of an establishment because they don’t have the relevant skill required to drive the workforce of the organization and also if an employee of an organization is not trained to level up their skill to match up to the role they are being hired for then they will have no contribution to the"
+
+    const vmcContents = [
+
+        {
+            titleofText: "Important of Skill Acquisition",
+            paragraphText: "Nigeria is a country where million number of students graduates yearly and when it comes to issue of employment, only 20% of them got employed and there is no assurance of job for the rest of the 80% and that sequence continue",
+            buttonText: "Read more",
+            buttonUr: "/important-of-skill-acquisition"
+        },
+        {
+            titleofText: "Skill is a powerful instrument",
+            paragraphText: "Skill is a powerful instrument in the sense that, it equipped or empower people and improves their social acceptance. Skill is a something that is complement by a good economic growth and improve job employment. Nigeria is moving towardsthe verge of being a developed country with the needs for a greater number of skillful and intelligent people",
+            buttonText: "Read more",
+            buttonUr: "/skill-is-a-powerful-instrument"
+        },
+        {
+            titleofText: "Lack of Skill in a Workplace",
+            paragraphText: "Unskilled employ can diminish the success of an establishment because they don’t have the relevant skill required to drive the workforce of the organization and also if an employee of an organization is not trained to level up their skill to match up to the role they are being hired for then they will have no contribution to the",
+            buttonText: "Read more",
+            buttonUr: "/lack-of-skill-in-a-workplace"
+        }
     ]
 
     return (
@@ -17,24 +34,16 @@ const Vmc = () => {
                             <h3 className="main-heading danger text-center mb-4">Important &#160;&#160;&#160;| &#160;&#160;&#160; Instruement &#160;&#160;&#160;| &#160;&#160;&#160; Workplace</h3>
                             <div className="underline mb-4 mx-auto"></div>
                             <div className='row'>
-                                <TextComponent
-                                    titleofText={"Important of Skill Acquisition"}
-                                    paragraphText={vmcText[0].substring(0, 200)}
-                                    buttonText={"Read more"}
-                                    buttonUr={"/important-of-skill-acquisition"}
-                                />
-                                <TextComponent
-                                    titleofText={"Skill is a powerful instrument"}
-                                    paragraphText={vmcText[1].substring(0, 200)}
-                                    buttonText={"Read more"}
-                                    buttonUr={"/skill-is-a-powerful-instrument"}
-                                />
-                                <TextComponent
-                                    titleofText={"Lack of Skill in a Workplace"}
-                                    paragraphText={vmcText[2].substring(0, 200)}
-                                    buttonText={"Read more"}
-                                    buttonUr={"/lack-of-skill-in-a-workplace"}
-                                />
+                                {
+                                    vmcContents.map((vmcContent) => (
+                                        <TextComponent
+                                            titleofText={vmcContent.titleofText}
+                                            paragraphText={vmcContent.paragraphText.substring(0, 200)}
+                                            buttonText={vmcContent.buttonText}
+                                            buttonUr={vmcContent.buttonUr}
+                                        />
+                                    ))
+                                }
                             </div>
                         </div>
                     </div>
